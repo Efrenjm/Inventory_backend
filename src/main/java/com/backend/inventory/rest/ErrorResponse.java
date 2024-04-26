94257944736736f43a59bdb468ae813b79bd6faa
Efren Alejandro Jimenez Mukul <efrenjimenez_96@hotmail.com>
2024-04-26 02:27:46 -0600

    Add Error handler and error response schema

---
+++ src/main/java/com/backend/inventory/rest/ErrorResponse.java
@@ -0,0 +1,65 @@
+package com.backend.inventory.rest;
+
+import org.springframework.http.HttpStatusCode;
+
+import java.time.OffsetDateTime;
+
+public class ErrorResponse {
+    private OffsetDateTime timestamp;
+    private HttpStatusCode status;
+    private String error;
+    private String message;
+    private String path;
+
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(HttpStatusCode status, String message, String path) {
+        this.timestamp = OffsetDateTime.now();
+        this.status = status;
+        this.message = message;
+        this.path = path;
+    }
+
+    public OffsetDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(OffsetDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public HttpStatusCode getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatusCode status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorResponse{" +
+                "timestamp=" + timestamp +
+                ", status=" + status +
+                ", message='" + message + '\'' +
+                ", path='" + path + '\'' +
+                '}';
+    }
+}
